b1dbd6bc32f3802a211e5d15ce891f74181231d0
Elisangela Ferreira <lisasf19@gmail.com>
2019-11-05 23:03:58 -0400

    Projeto Criado.

---
+++ src/main/java/br/gov/mt/pjc/BackEndMusicaApplication.java
@@ -0,0 +1,13 @@
+package br.gov.mt.pjc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndMusicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndMusicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/mt/pjc/BackEndMusicaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.mt.pjc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndMusicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
