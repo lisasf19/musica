7817e7fe76ccd27d178b2e4889653fbae79e8734
Elisangela Ferreira <lisasf19@gmail.com>
2019-11-06 21:49:09 -0400

    Método alterar Artista codificado

--- src/main/java/br/gov/mt/pjc/service/ArtistaService.java
+++ src/main/java/br/gov/mt/pjc/service/ArtistaService.java
@@ -1,7 +1,9 @@
 package br.gov.mt.pjc.service;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +23,28 @@ public class ArtistaService {
 	
 	public void apagarArtista(Integer id) {
 		artistaRepository.deleteById(id);
+	}
+	
+	public Artista buscarPorId(Integer id) {
+		Optional<Artista> artistaOptional = artistaRepository.findById(id);
+		//verifica se foi encontrado registro
+		if(artistaOptional.isPresent()) {
+			return artistaOptional.get();
+		}else {
+			throw new RuntimeException("Artista não cadastrado. Código não encontrado.");
+		}		
+	}
+	
+	public Artista alterarArtista(Integer id, Artista artista) {
+		//verifica se o registro a ser alterado já existe no banco de dados
+		Artista artistaSalvo = this.buscarPorId(id);
+		
+		if(artistaSalvo != null) {
+			BeanUtils.copyProperties(artista, artistaSalvo, "id");
+			return artistaRepository.save(artistaSalvo);
+		}else {
+			throw new RuntimeException("Artista não encontrado.");
+		}
 	}
 
 }
