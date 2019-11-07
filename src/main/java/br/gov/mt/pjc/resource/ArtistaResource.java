14c92c32b7dae3634fc96e36c63a64814c435e94
Elisangela Ferreira <lisasf19@gmail.com>
2019-11-06 23:46:12 -0400

    Resource de Artista criado com os métodos CRUD.

--- src/main/java/br/gov/mt/pjc/resource/ArtistaResource.java
+++ src/main/java/br/gov/mt/pjc/resource/ArtistaResource.java
@@ -2,8 +2,18 @@ package br.gov.mt.pjc.resource;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +32,27 @@ public class ArtistaResource {
 		return artistaService.buscarTodos();
 	}
 	
+	@PostMapping
+	public Artista cadastrarArtista(@Valid @RequestBody Artista artista) {
+		return artistaService.salvar(artista);
+	}
+	
+	@PutMapping("/{id}")
+	public Artista alterarArtista(@Valid @RequestBody Artista artista,
+			@PathVariable("id") Integer id) {
+		return artistaService.alterarArtista(id, artista);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> deletarArtista(@PathVariable Integer id){
+		try {
+			artistaService.apagarArtista(id);
+			return ResponseEntity.status(HttpStatus.OK)
+					.body("Artista removido com sucesso.");
+		}catch (EmptyResultDataAccessException e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+					.body("Artista informado não encontrado. \nErro de origem:"+ e.getMessage());
+		}
+	}
 
 }
