ed16926577f0e11bf41ffe00975430ef816cd3ae
Elisangela Ferreira <lisasf19@gmail.com>
2019-11-06 20:47:23 -0400

    Implementação das classes mapeadas do banco (Artista e Ambum); alteração nas informações de conexão com a base;

---
+++ src/main/java/br/gov/mt/pjc/model/Album.java
@@ -0,0 +1,98 @@
+package br.gov.mt.pjc.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name= "Album")
+public class Album implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(name = "titulo")
+	private String titulo;
+	
+	@ManyToOne
+	@JoinColumns(@JoinColumn(name = "id_artista", referencedColumnName = "id"))
+	private Artista artista;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public Artista getArtista() {
+		return artista;
+	}
+
+	public void setArtista(Artista artista) {
+		this.artista = artista;
+	}
+
+	public Album(Integer id, String titulo, Artista artista) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.artista = artista;
+	}
+
+	public Album() {
+		super();
+	}
+
+	@Override
+	public String toString() {
+		return "Album [id=" + id + ", titulo=" + titulo + ", artista=" + artista + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Album other = (Album) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
---
+++ src/main/java/br/gov/mt/pjc/model/Artista.java
@@ -0,0 +1,90 @@
+package br.gov.mt.pjc.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Artista")
+public class Artista implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(name = "nome")
+	private String nome;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Artista other = (Artista) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Artista [id=" + id + ", nome=" + nome + "]";
+	}
+
+	public Artista() {
+		super();
+	}
+
+	public Artista(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+	
+	public  boolean verificaCamposObrigatorios() {
+		if(getNome().isEmpty() || getNome()== null) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+}
